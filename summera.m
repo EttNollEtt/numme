5cad339b3e9f503cd5ec39b20e2450983b99191f
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2018-04-25 10:00:35 +0200

    Delete summera.m

--- summera.m
+++
@@ -1,8 +0,0 @@
-function [sura] = summera(vek)
-smmera = zeros(length(vek), 1);
-for x=1:length(vek)
-    for a=1:14
-        smmera(x, 1) = smmera(x,1) + sum(vek(x,a));
-    end
-end
-sura = smmera;
