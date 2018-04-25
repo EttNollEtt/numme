6f9dc2f6f8b9af9d2431e7135943de5f74357c03
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2018-04-25 09:58:58 +0200

    Delete RKsteg.m

--- RKsteg.m
+++
@@ -1,8 +0,0 @@
-function [Tut, Yut] = RKsteg(yprim, t, y, h)
-    k1 = h*feval(yprim, t, y);
-    k2 = h*feval(yprim, t+h/2, y+k1/2);
-    k3 = h*feval(yprim, t+h/2, y+k2/2);
-    k4 = h*feval(yprim, t+h, y+k3);
-    ny = k1/6 + k2/3 + k3/3 + k4/6;
-Yut = (y+ny);
-Tut = t+h;
